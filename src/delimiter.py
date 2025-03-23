37c7b3b8c3a8deb37cd311c81ca71a7c216863fb
Jon Castro <jonattancast@gmail.com>
2025-03-23 17:01:46 -0400

    regexes look scary

--- src/delimiter.py
+++ src/delimiter.py
@@ -8,11 +8,17 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type != text_type.TEXT:
            new_list.append(node)
+           continue
+    split_nodes = []
+    sections = node.text.split(delimiter)
+    if len(sections) % 2 == 0:
+        raise ValueError("invalid martkdown")
+    for i in range(len(sections)):
+        if sections[i] == "":
+            continue
+        if i % 2 == 0:
+            split_nodes.append(TextNode(sections[i], TextType.TEXT))
         else:
-            text = node.text.split(delimiter)
-               
-        
-
-
-    
+            split_nodes.append(TextNode(sections[i], text_type))
+    new_list.extend(split_nodes)
     return new_list
